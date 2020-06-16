2ea3fc86e5bf21ecad2b0003b975a4f289be8378
AndreySlabov <65009440+AndreySlabov@users.noreply.github.com>
2020-06-16 16:56:13 +0300

    Add files via upload

---
+++ CS/WPFDataGridGettingStartedNETCore/Models/CustomerDemographic.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DXGridGetStartedCore.Models {
+    public partial class CustomerDemographic {
+        public string CustomerTypeId { get; set; }
+        public string CustomerDesc { get; set; }
+    }
+}
