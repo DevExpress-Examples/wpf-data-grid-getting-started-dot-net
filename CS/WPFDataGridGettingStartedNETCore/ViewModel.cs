467791d1afcee37bb2d8876ada0595b81c762a31
AndreySlabov <65009440+AndreySlabov@users.noreply.github.com>
2020-06-17 12:12:58 +0300

    Add files via upload

---
+++ CS/WPFDataGridGettingStartedNETCore/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.EntityFrameworkCore;
+using DevExpress.Mvvm;
+using DevExpress.Mvvm.Native;
+using WPFDataGridGettingStartedNETCore.Models;
+
+namespace WPFDataGridGettingStartedNETCore {
+    public class ViewModel : ViewModelBase {
+        NorthwindEntities northwindDBContext;
+        public ViewModel() {
+            if (IsInDesignMode) {
+                Orders = new ObservableCollection<Order>();
+                Shippers = new ObservableCollection<Shipper>();
+            }
+            else {
+                northwindDBContext = new NorthwindEntities();
+
+                northwindDBContext.Orders.Load();
+                Orders = northwindDBContext.Orders.ToObservableCollection();
+
+                northwindDBContext.Shippers.Load();
+                Shippers = northwindDBContext.Shippers.ToObservableCollection();
+            }
+            ValidateAndSaveCommand = new DelegateCommand(ValidateAndSave);
+        }
+        public ObservableCollection<Order> Orders { get; }
+        public ObservableCollection<Shipper> Shippers { get; }
+        public DelegateCommand ValidateAndSaveCommand { get; }
+        void ValidateAndSave() {
+            northwindDBContext.SaveChanges();
+        }
+    }
+}
