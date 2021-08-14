cde1812083c20a84dd44e1c037cceec35a356159
aliev.faramoz <faramoz.aliev@devexpress.com>
2021-08-15 02:12:45 +0300

    update

--- CS/WPFDataGridGettingStartedNETCore/App.xaml.cs
+++ CS/WPFDataGridGettingStartedNETCore/App.xaml.cs
@@ -1,19 +1,5 @@
-﻿using DevExpress.Xpf.Core;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-namespace WPFDataGridGettingStartedNETCore
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-
-    }
+namespace WPFDataGridGettingStartedNETCore {
+    public partial class App : Application { }
 }
--- CS/WPFDataGridGettingStartedNETCore/ViewModel.cs
+++ CS/WPFDataGridGettingStartedNETCore/ViewModel.cs
@@ -1,28 +1,14 @@
-﻿using System.Collections.ObjectModel;
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.Xpf;
 using Microsoft.EntityFrameworkCore;
-using DevExpress.Mvvm;
-using DevExpress.Mvvm.Native;
+using System.Collections.ObjectModel;
 using WPFDataGridGettingStartedNETCore.Models;
 
 namespace WPFDataGridGettingStartedNETCore {
     public class ViewModel : ViewModelBase {
         NorthwindEntities northwindDBContext;
-        public ViewModel() {
-            if (IsInDesignMode) {
-                Orders = new ObservableCollection<Order>();
-                Shippers = new ObservableCollection<Shipper>();
-            }
-            else {
-                northwindDBContext = new NorthwindEntities();
 
-                northwindDBContext.Orders.Load();
-                Orders = northwindDBContext.Orders.ToObservableCollection();
-
-                northwindDBContext.Shippers.Load();
-                Shippers = northwindDBContext.Shippers.ToObservableCollection();
-            }
-            ValidateAndSaveCommand = new DelegateCommand(ValidateAndSave);
-        }
         public ObservableCollection<Order> Orders {
             get => GetValue<ObservableCollection<Order>>();
             private set => SetValue(value);
@@ -31,8 +17,19 @@ namespace WPFDataGridGettingStartedNETCore {
             get => GetValue<ObservableCollection<Shipper>>();
             private set => SetValue(value);
         }
-        public DelegateCommand ValidateAndSaveCommand { get; }
-        void ValidateAndSave() {
+
+        public ViewModel() {
+            northwindDBContext = new NorthwindEntities();
+
+            northwindDBContext.Orders.Load();
+            Orders = northwindDBContext.Orders.ToObservableCollection();
+
+            northwindDBContext.Shippers.Load();
+            Shippers = northwindDBContext.Shippers.ToObservableCollection();
+        }
+
+        [Command]
+        public void ValidateAndSave(RowValidationArgs args) {
             northwindDBContext.SaveChanges();
         }
     }
